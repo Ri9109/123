998f71adae09f0fc571d1d0c0d2d671571f47554
Ri9109 <148952375+Ri9109@users.noreply.github.com>
2023-12-01 19:12:10 +1000

    Add files via upload

---
+++ Trampoline.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour
+{
+    public float jumpFactor = 10f;
+    void OnTriggerEnter(Collider other)
+    {
+        other.GetComponent<Jump>().jumpStrength *= jumpFactor;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        other.GetComponent<Jump>().jumpStrength /= jumpFactor;
+    }
+
+}
